0535b25a8c0ec67f529a7807c799a4af40b211d2
Taylor Johnson <taylor.christian.johnson@gmail.com>
2017-10-12 16:50:16 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const { mongoose } = require('./db/mongoose')
+const { ObjectId } = require('mongodb')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
 
@@ -31,6 +32,26 @@ app.get('/todos', (req, res) => {
       res.status(400).send(e)
     }
   )
+})
+
+// GET /todos/{{id}}
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(req.params.id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+      res.status(200).send({ todo })
+    })
+    .catch(e => {
+      res.status(400).send(e)
+    })
 })
 
 app.listen(3000, () => {
