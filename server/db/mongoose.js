304f1d1bc7537ba8efa9fa1de2f1f2221bbf56f4
Taylor Johnson <taylor.christian.johnson@gmail.com>
2017-10-12 18:02:01 -0400

    Toggle mongodb connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,18 @@
 let mongoose = require('mongoose')
 let db = require('./dbConfig.js')
 
+let port = process.env.PORT || 3000,
+  mCon
+
 mongoose.Promise = global.Promise
-mongoose.connect(`mongodb://${db.user}:${db.pw}@ds117605.mlab.com:17605/tododb`)
+
+if (port === 3000) {
+  mCon = 'mongodb://localhost:27017/TodoApp'
+} else {
+  mCon = `mongodb://${db.user}:${db.pw}@ds117605.mlab.com:17605/tododb`
+}
+//mongoose.connect(`mongodb://${db.user}:${db.pw}@ds117605.mlab.com:17605/tododb`)
 //mongoose.connect('mongodb://localhost:27017/TodoApp')
+mongoose.connect(mCon)
 
 module.exports = { mongoose }
