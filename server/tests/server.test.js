17290486841c810ab063c6c24e710a6e2b9c3a4d
Taylor Johnson <taylor.christian.johnson@gmail.com>
2017-10-12 17:05:18 -0400

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,17 @@
 const expect = require('expect')
 const request = require('supertest')
+const { ObjectId } = require('mongodb')
 
 var { app } = require('./../server')
 const { Todo } = require('./../models/todo')
 
 const todos = [
   {
+    _id: new ObjectId(),
     text: 'first test todo'
   },
   {
+    _id: new ObjectId(),
     text: 'second test todo'
   }
 ]
@@ -77,6 +80,32 @@ describe('GET /todos', () => {
       .expect(res => {
         expect(res.body.todos.length).toBe(2)
       })
+      .end(done)
+  })
+})
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(todos[0].text)
+      })
+      .end(done)
+  })
+
+  it('should return a 404 if todo not found', done => {
+    request(app)
+      .get(`/todos/${new ObjectId().toHexString()}`)
+      .expect(404)
+      .end(done)
+  })
+
+  it('should return a 404 if invalid todo id', done => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
       .end(done)
   })
 })
