effdfb8442343dd5fa7da89805b50635b5d57f11
Taylor Johnson <taylor.christian.johnson@gmail.com>
2017-10-30 16:42:11 -0400

    Add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -304,3 +304,27 @@ describe('POST /user/login', () => {
       })
   })
 })
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', done => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect(res => {
+        expect(res.headers['x-auth']).toNotExist()
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        User.findById(users[0]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0)
+            done()
+          })
+          .catch(e => done(e))
+      })
+  })
+})
