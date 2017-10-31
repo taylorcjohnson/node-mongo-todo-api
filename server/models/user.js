f7f743b3310e951f306ed9fa844c3baaddb785df
Taylor Johnson <taylor.christian.johnson@gmail.com>
2017-10-31 17:01:38 -0400

    Update config to user json file

--- server/models/user.js
+++ server/models/user.js
@@ -47,7 +47,7 @@ UserSchema.methods.generateAuthToken = function() {
   let user = this
   let access = 'auth'
   let token = jwt
-    .sign({ _id: user._id.toHexString(), access }, 'abc123')
+    .sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET)
     .toString()
 
   user.tokens.push({ access, token })
@@ -73,7 +73,7 @@ UserSchema.statics.findByToken = function(token) {
   let decoded
 
   try {
-    decoded = jwt.verify(token, 'abc123')
+    decoded = jwt.verify(token, process.env.JWT_SECRET)
   } catch (e) {
     return Promise.reject()
   }
